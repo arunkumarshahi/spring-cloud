3bcd194d3366b40b1e5cd64779d354ef8e82086d
arunkumarshahi@gmail.com <arunkumarshahi@gmail.com>
2020-03-11 23:25:04 +0530

    added eureka server

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
@@ -1,12 +1,15 @@
 package com.example.currencyconversionservice.service;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.currencyconversionservice.model.CurrencyValueBean;
 
-@FeignClient(name="currency-exchange-service",url="localhost:8100")
+//@FeignClient(name="currency-exchange-service",url="localhost:8100")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 public CurrencyValueBean retrieveCurrencyValue(@PathVariable String from, @PathVariable String to);
--- currency-exchange-service/src/main/java/com/example/currencyexchanageservice/CurrencyExchanageServiceApplication.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchanageservice/CurrencyExchanageServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.currencyexchanageservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CurrencyExchanageServiceApplication {
 
     public static void main(String[] args) {
